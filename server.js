b54bed792f64938d88899dc32ae4b426f50a2102
Deepak Kumar Deep <78579328+codewithdkd@users.noreply.github.com>
2025-09-29 15:15:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`✅ Server running on port ${PORT}`);
+});
 
 // Middleware
 app.use(bodyParser.json());
