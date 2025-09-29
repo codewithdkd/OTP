fb84a7b5a081b45f77e86d77f6c8431c1c87b6a9
Deepak Kumar Deep <78579328+codewithdkd@users.noreply.github.com>
2025-09-29 15:23:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`✅ Server running on port ${PORT}`);
 });
