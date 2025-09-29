edfc279786dcd18784bb46327974a44ac442ded5
Deepak Kumar Deep <78579328+codewithdkd@users.noreply.github.com>
2025-09-29 15:53:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require("path");
 const app = express();
 
 // Railway assigns port via environment variable
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(bodyParser.json());
